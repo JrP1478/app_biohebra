b979688c07e9eaffeee725d8f2a8482466abde49
Junior Pérez <jrplox987@gmail.com>
2026-05-05 22:24:37 -0500

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,20 @@
 require('dotenv').config();
 
-module.exports = {
-    development: {
-        client: 'sqlite3',
-        connection: {
-            filename: process.env.DB_FILENAME || './database.sqlite'
-        },
-        useNullAsDefault: true,
-        migrations: {
-            directory: './src/database/migrations'
-        },
-        seeds: {
-            directory: './src/database/seeds'
-        }
+const config = {
+    client: 'sqlite3',
+    connection: {
+        filename: process.env.DB_FILENAME || './database.sqlite'
+    },
+    useNullAsDefault: true,
+    migrations: {
+        directory: './src/database/migrations'
+    },
+    seeds: {
+        directory: './src/database/seeds'
     }
+};
+
+module.exports = {
+    development: config,
+    production: config
 };
